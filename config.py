00b3b97c0f412a7f0e8c6b453d5065f39ef5536c
Syllstars <102628630+Syllstars@users.noreply.github.com>
2022-06-14 09:11:50 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+class Config():
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+
+    MAIL_DEBUG = False
+    MAIL_SUPPRESS_SEND = False
+
+    MAIL_USERNAME = 'mpontlevoy2001@gmail.com'
+    MAIL_PASSWORD = 'bbujdcouojswajpj'
+
+    MAIL_DEFAULT_SENDER = ('MMOGC -- Générateur de classe', 'maxime.pontlevoy@orange.fr')
+    MAIL_MAX_EMAILS = None 
+    MAIL_ASCII_ATTACHMENTS = False
+
+
+
+
