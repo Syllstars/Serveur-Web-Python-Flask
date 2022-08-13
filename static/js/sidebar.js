a0d280998111036f2a1f5ba9e5329854d5975e14
Maxime PONTLEVOY <102628630+Syllstars@users.noreply.github.com>
2022-08-13 12:03:22 +0200

    Add files via upload

---
+++ static/js/sidebar.js
@@ -0,0 +1,24 @@
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+    section = body.querySelector(".home");
+
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+    section.classList.toggle("open");
+})
+
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Light mode";
+    } else {
+        modeText.innerText = "Dark mode";
+
+    }
+});
